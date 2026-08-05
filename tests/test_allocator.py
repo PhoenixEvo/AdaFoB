344eb8a2ac280fb3d76b16d6778f7861b390cee9
Nguyễn Nhật Phát <nhatphatnguyen.ai@gmail.com>
2026-08-05 21:56:31 +0700

    Fix imports in test_allocator

--- tests/test_allocator.py
+++ tests/test_allocator.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import numpy as np
 from third_party.FoB_SAM.models.FoB import FewShotSeg
